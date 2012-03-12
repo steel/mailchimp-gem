69cabebbcbfae03c31aa36883cc4cebe2a9c35c0
Daniel Rice <drice@highgroove.com>
2012-03-12 16:22:26 -0400

    [#26288755] Updating the exporter test to include all export api methods

--- test/integration/export_test.rb
+++ test/integration/export_test.rb
@@ -12,12 +12,23 @@ class ExportIntegrationTest < Test::Unit::TestCase
 ["chimpy@example.com","Daniel","Rice","00Qd0000006AjjtEAC","html",2,"",null,"2012-03-08 20:52:31","173.160.74.121",null,null,null,null,null,null,null,"2012-03-12 17:45:20"]
 ["wolfbrain@example.com","George P","Burdell","00Qd0000006AjjuEAC","html",2,"",null,"2012-03-08 20:53:10","173.160.74.121",null,null,null,null,null,null,null,"2012-03-12 17:45:22"]'
     )
-    #expected_request = {"apikey"=>"abc123-us1"}
     
     m = Mailchimp::Export.new('abc123-us2')
     response = m.list(:id => 'xxxxxxxx')
     
-    response.each {|response_row| puts response_row } 
+    assert_kind_of Enumerator, response
     
+  end
+  
+  should "get campaign subscriber activity" do
+
+    FakeWeb.register_uri(
+      :post,
+      'http://us2.api.mailchimp.com/export/1.0/campaignSubscriberActivity/',
+      body: '{"highgroove@example.com":[{"action":"open","timestamp":"2012-03-12 20:14:17","url":null,"ip":"184.77.22.196"}]} {"wolfbrain@example.com":[{"action":"open","timestamp":"2012-03-12 20:14:20","url":null,"ip":"184.77.22.196"}]}'.to_json
+    )
+    response = Mailchimp::Export.new('abc123-us2').campaign_subscriber_activity(:id => 'xxxxxxxx')
+    
+    assert_kind_of Enumerator, response
   end
 end
